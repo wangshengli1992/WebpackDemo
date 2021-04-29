dc01f8825a77ed8d84f3fec4cfacd9f9a26eaa00
wangshengli <wangshengli.@ecqun.com>
2021-04-29 19:42:37 +0800

    添加react babel

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -4,6 +4,21 @@ const path = require('path');
  * @type {import('webpack').Configuration}
  */
 const config = {
-    
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
 };
 module.exports = config;
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="myApp">
+            hello,world
+        </div>
+    );
+}
+
+export default App;
