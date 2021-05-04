3b830b7466ae102fe3367e7653f0c45e12be34c0
victory <1257863270@qq.com>
2021-05-04 22:50:07 +0800

    feat: 支持ts tsx

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -5,96 +5,98 @@ const { webpack } = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { options } = require('less');
 
-
 /**
  * webpack智能提示功能
  * @type {import('webpack').Configuration}
  */
 const config = {
-    entry: {
-        app: './src/index.js',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, '../dist')
-    },
-    devtool: 'eval-cheap-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devtool: 'eval-cheap-source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          'postcss-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]-[hash:base64:5]',
+              },
             },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                    },
-                    'postcss-loader',
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]-[hash:base64:5]'
-                            },
-                        }
-                    },
-                    'postcss-loader',
-                    'less-loader',
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'images/',
-                        limit: 100000,
-                    }
-                }
-                ]
-            },
-            {
-                test: /\.(eot|ttf|svg|woff|woff2)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name]_[hash].[ext]',
-                        outputPath: 'font/'
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            filename: 'index.html',
-            inject: 'body',
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-            }
-        }),
-        new CleanWebpackPlugin({
-            dry: false,
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash:8].css',
-            chunkFilename: '[id].[hash:8].css'
-        }),
+          },
+          'postcss-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            limit: 100000,
+          },
+        },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|svg|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'font/',
+          },
+        },
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    new CleanWebpackPlugin({
+      dry: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css',
+      chunkFilename: '[id].[hash:8].css',
+    }),
+  ],
 };
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from "./app.jsx";
-
+import App from './app';
 
 const render = () => {
-    ReactDom.render(<App />, document.querySelector('#root'));
-}
+  ReactDom.render(<App />, document.querySelector('#root'));
+};
 
 render();
 
 const m = module;
 if (m.hot) {
-    m.hot.accept(['./app.js'], () => {
-        render('ss')
-    });
+  m.hot.accept(['./app.js'], () => {
+    render('ss');
+  });
 }
