3252e1a164636d2779b8c8d34f8817e99acdaa65
victory <1257863270@qq.com>
2021-05-04 20:33:29 +0800

    测试提交

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,15 @@ import './index.css';
 import style from './app.less';
 import ImgHeader from '../static/images/header.jpg';
 
-const App = () => {
-  return (
-    <div className={style.myApp} style={{ width: 300, height: 300, background: 'gray' }}>
-      <span>
-        hello
-      </span>
-      <h1>霓虹</h1>
-      <img src={ImgHeader} alt="" />
-      <i className="iconfont">&#xe60a;</i>
+const App = () => (
+  <div className={style.myApp} style={{ width: 300, height: 300, background: 'gray' }}>
+    <span>
+      hello2
+    </span>
+    <h1>霓虹</h1>
+    <img src={ImgHeader} alt="" />
+    <i className="iconfont">&#xe60a;</i>
 
-    </div>
-  );
-};
+  </div>
+);
 export default App;
